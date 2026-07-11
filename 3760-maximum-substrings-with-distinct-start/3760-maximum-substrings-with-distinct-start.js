f973bdf70476d356b5587c536cf44f5918ca02e5
Ashik <lllashik2@gmail.com>
2026-07-11 10:26:31 +0530

    Time: 58 ms (12.69%), Space: 69.6 MB (6.09%) - LeetHub

--- 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.js
+++ 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.js
@@ -4,13 +4,16 @@
  */
 var maxDistinct = function(s) {
     const count ={}
-    const arr = s.split('');
-    console.log(arr);
-    arr.forEach((x)=>{
-        if(!count[x]){
-            count[x]=true
-            console.log(x)
-        }
-    })
-    return Object.keys(count).length
+    const arr = s.split('').reduce((acc,x)=>{
+        acc[x]=true;
+        return acc
+    },{})
+    // console.log(arr);
+    // arr.forEach((x)=>{
+    //     if(!count[x]){
+    //         count[x]=true
+    //         console.log(x)
+    //     }
+    // })
+    return Object.keys(arr).length
 };
