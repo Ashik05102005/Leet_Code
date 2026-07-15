1cabbc5f5b287e8ed301cf97de8f61597fc6202b
Ashik <lllashik2@gmail.com>
2026-07-15 09:37:37 +0530

    Time: 17 ms (2.97%), Space: 62.7 MB (7.08%) - LeetHub

---
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var buildArray = function(nums) {
+    console.log(nums)
+    let arr = []
+    for(let i = 0 ; i<nums.length ; i++ ){
+        arr[i] = nums[nums[i]];
+    }
+    return arr ;
+};
