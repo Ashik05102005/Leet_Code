64bbd1e04d67fae09756de33a3e5c32e67571c04
Ashik <lllashik2@gmail.com>
2026-07-15 09:50:16 +0530

    Time: 0 ms (100%), Space: 55.2 MB (63.12%) - LeetHub

--- 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.js
+++ 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.js
@@ -6,13 +6,10 @@
 var differenceOfSums = function(n, m) {
     let sum =0;
     for(let i=1; i<=n ; i++){
-        // console.log(i);
         if(i%m!==0){
-            // console.log(i)
             sum += i ; 
         }
         else{
-            console.log(i)
             sum -= i
         }
     }
