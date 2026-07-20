714ef6bcaacdae175fcb616386569fcb497c6f35
Ashik <lllashik2@gmail.com>
2026-07-20 18:33:20 +0530

    Time: 5 ms (22.82%), Space: 58.3 MB (33.45%) - LeetHub

---
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var leftRightDifference = function(nums) {
+    let first = 0 , firstArray = [0] ;
+    let seccond = 0 , seccondArray = [0] ;
+    for(let i = 0; i < nums.length-1 ; i++ ){
+        first+=nums[i] ;
+        firstArray.push(first);
+        seccond+=nums[nums.length-1-i];
+        seccondArray.push(seccond)
+    }
+    seccondArray.reverse()
+    return  firstArray.map(( item , index)=>Math.abs(item -seccondArray[index]))
+    console.log(res)
+    console.log(resView)
+
+};
