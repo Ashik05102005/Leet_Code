461e878baae53558c35418d26d6a9afdfecc4bdc
Ashik <lllashik2@gmail.com>
2026-07-22 10:18:25 +0530

    Time: 78 ms (6.7%), Space: 67.5 MB (93.83%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -4,7 +4,6 @@
  */
 var containsDuplicate = function(nums) {
     nums.sort((a,b)=>a-b);
-    console.log(nums);
     for(let i = 0 ; i<nums.length-1 ; i++ ){
         if(nums[i]===nums[i+1]){
             return true
