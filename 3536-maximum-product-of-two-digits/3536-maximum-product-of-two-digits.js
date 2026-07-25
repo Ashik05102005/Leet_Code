2fcfe18783e097fc82960bcb870695dd3aad3d2e
Ashik <lllashik2@gmail.com>
2026-07-25 09:27:48 +0530

    Time: 122 ms (6.38%), Space: 64.8 MB (6.38%) - LeetHub

---
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var maxProduct = function(n) {
+    let resArray = []
+    const numArray = n.toString().split("").map(x=>Number(x));
+    for(let i = 0 ; i < numArray.length ; i++ ){
+        for(let j = i+1 ; j<numArray.length ; j++ ){
+            console.log(numArray[i]*numArray[j])
+            console.log(numArray[i] , numArray[j])
+            resArray.push(numArray[i]*numArray[j])
+        }
+    }
+    return Math.max(...resArray)
+};
