e850ea02e357e348c505f5408833b3d4ad398dde
Ashik <lllashik2@gmail.com>
2026-07-29 11:50:32 +0530

    Time: 26 ms (7.06%), Space: 58.8 MB (11.98%) - LeetHub

---
+++ 3541-find-most-frequent-vowel-and-consonant/3541-find-most-frequent-vowel-and-consonant.js
@@ -0,0 +1,39 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var maxFreqSum = function(s) {
+    const vowels = ['a','e','i','o','u'] ;
+    let charObj = {}
+    let vowelsCount=[]  , consonantsCount = [] ;
+    let vowelMax = 0 , consonantsMax = 0;
+    s.split("").forEach((char)=>{
+        if(charObj[char]){
+            charObj[char]++
+        }
+        else{
+            charObj[char] =1
+        }
+    });
+    // console.log(charObj)
+    for (let i in charObj){
+      if(vowels.includes(i))  {
+        // vowelsCount.push(charObj[i])
+        if(vowelMax<charObj[i]){
+            vowelMax=charObj[i]
+        }     
+      }
+      else{
+        //  consonantsCount.push(charObj[i])
+         if(consonantsMax<charObj[i]){
+            consonantsMax=charObj[i]
+        }
+      }
+    }
+    // console.log("vowels->",vowelsCount)
+    // console.log("consonents->",consonantsCount)
+    // const res = (Math.max(...vowelsCount) + Math.max(...consonantsCount));
+    console.log( consonantsMax + vowelMax );
+    const res = consonantsMax + vowelMax ;
+    return res ;
+};
