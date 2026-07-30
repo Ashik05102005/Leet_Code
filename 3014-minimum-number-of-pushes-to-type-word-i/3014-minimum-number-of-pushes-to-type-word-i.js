71a223eb92e03a737755d5b993531a7844e8d1a8
Ashik <lllashik2@gmail.com>
2026-07-30 09:42:41 +0530

    Time: 26 ms (3.33%), Space: 57.1 MB (10%) - LeetHub

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} word
+ * @return {number}
+ */
+var minimumPushes = function(word) {
+    // console.log(word)
+    count = 0;
+    const wordArray = word.split("")
+    const resArr = []
+    for(i = 0 ; i < wordArray.length/8; i++){
+        console.log("_____________",i)
+        const Arr = [];
+        for(j = i*8 ; j<(i+1)*8 ; j++ ){
+            Arr.push(wordArray[j])
+        }
+        resArr.push(Arr.join(""))
+    }
+    console.log(resArr);
+    const res = resArr.reduce((acc , item , index)=>acc+=item.length*(index+1) ,0 );
+    return res
+};
