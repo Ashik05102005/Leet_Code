f628987209e37d289a5bde20443cc432b51fd6c0
Ashik <lllashik2@gmail.com>
2026-07-30 09:43:08 +0530

    Time: 1 ms (30%), Space: 55.5 MB (20%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.js
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.js
@@ -8,14 +8,13 @@ var minimumPushes = function(word) {
     const wordArray = word.split("")
     const resArr = []
     for(i = 0 ; i < wordArray.length/8; i++){
-        console.log("_____________",i)
+
         const Arr = [];
         for(j = i*8 ; j<(i+1)*8 ; j++ ){
             Arr.push(wordArray[j])
         }
         resArr.push(Arr.join(""))
     }
-    console.log(resArr);
     const res = resArr.reduce((acc , item , index)=>acc+=item.length*(index+1) ,0 );
     return res
 };
