c0cd33bcb8340c380f7db7985562167ff62760af
Ashik <lllashik2@gmail.com>
2026-08-04 09:30:39 +0530

    Time: 1 ms (99.21%), Space: 58.7 MB (38.58%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.js
+++ 3731-find-missing-elements/3731-find-missing-elements.js
@@ -3,10 +3,10 @@
  * @return {number[]}
  */
 var findMissingElements = function(nums) {
-    // const Max = ;
-    // const Min = Math.min(...nums);
+    const Max = Math.max(...nums);
+    const Min = Math.min(...nums);
     const res=[]
-    for(let i=Math.min(...nums);i<=Math.max(...nums);i++){
+    for(let i=Min;i<=Max;i++){
         if(!nums.includes(i)){
             res.push(i)
         }
