cd9b195a58c62eb4f29a429cec58585ad29e9a2f
Ashik <lllashik2@gmail.com>
2026-08-05 09:44:21 +0530

    Time: 3 ms (17.54%), Space: 55.9 MB (66.67%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.js
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var reverseDegree = function(s) {
-    // const alphabets = 'abcdefghijklmnopqrstuvwxyz'.split("").reverse();
-    return s.split("").reduce((acc,item,index)=>acc+=(('abcdefghijklmnopqrstuvwxyz'.split("").reverse().indexOf(item)+1)*(index+1)),0);
+    const alphabets = 'abcdefghijklmnopqrstuvwxyz'.split("").reverse();
+    return s.split("").reduce((acc,item,index)=>acc+=((alphabets.indexOf(item)+1)*(index+1)),0);
     
 };
