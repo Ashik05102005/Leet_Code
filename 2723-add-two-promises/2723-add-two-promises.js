58ba5d615023b69203c8151dc29520553e48b2c4
Ashik <lllashik2@gmail.com>
2026-08-07 09:17:32 +0530

    Time: 47 ms (86.01%), Space: 54.8 MB (11.35%) - LeetHub

--- 2723-add-two-promises/2723-add-two-promises.js
+++ 2723-add-two-promises/2723-add-two-promises.js
@@ -6,9 +6,9 @@
 var addTwoPromises = async function(promise1, promise2) {
     try{
     const p1 = await promise1;
-    console.log(p1)
+    // console.log(p1)
     const p2 = await promise2;
-    console.log(p2)
+    // console.log(p2)
     return p1 + p2
     }
     catch(error){
