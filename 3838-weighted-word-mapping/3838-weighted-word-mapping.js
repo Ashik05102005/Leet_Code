3f6280c8ad5a790deab5616b822ea2bc7fbbf24d
Ashik <lllashik2@gmail.com>
2026-08-20 09:55:32 +0530

    Time: 91 ms (7.87%), Space: 63.5 MB (8.75%) - LeetHub

---
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string[]} words
+ * @param {number[]} weights
+ * @return {string}
+ */
+var mapWordWeights = function(words, weights) {
+    const lettersArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    const reversed =  ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'].reverse();
+    let charObj = {}
+    lettersArray.forEach((x,index)=>{
+        charObj = {...charObj,[x]:weights[index]};
+    })
+    // console.log(charObj);
+    const res = words.map((x)=>(x.split("")
+                        .map((letter)=>{
+                                return charObj[letter]
+                            })
+    ))
+    const sum = res.map((mainItem)=> mainItem.reduce((acc,item)=>acc+=item))
+    const result = sum.map((item)=>{
+        const index = item%26;
+        console.log(index)
+        return reversed[index]
+    });
+    return result.join("")
+};
