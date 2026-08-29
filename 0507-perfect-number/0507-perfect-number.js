ab3565fd077f5d470ac5f3f8b1b5e72a4da08310
Ashik <lllashik2@gmail.com>
2026-08-29 09:47:57 +0530

    Time: 1051 ms (43.4%), Space: 53.1 MB (92.36%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.js
+++ 0507-perfect-number/0507-perfect-number.js
@@ -3,9 +3,9 @@
  * @return {boolean}
  */
 var checkPerfectNumber = function(num) {
-    const limit = Math.floor(num/2);
+    // const limit = Math.floor(num/2);
     let sum=0;
-    for(let i=1; i<=limit;i++){
+    for(let i=1; i<=Math.floor(num/2);i++){
         if(num%i===0){
             sum+=i
         }
