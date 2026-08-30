bf6d63df495c27c33e13f3319299549505cb817c
Ashik <lllashik2@gmail.com>
2026-08-30 13:48:49 +0530

    Time: 11 ms (78.91%), Space: 56.8 MB (60.16%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -0,0 +1,39 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    sObj = {};
+    tObj = {};
+    s.split("").forEach(x=>{
+        if(sObj[x]){
+            sObj[x]=sObj[x]+1;
+        }
+        else{
+            sObj[x]=1
+        }
+    });
+    t.split("").forEach(x=>{
+        if(tObj[x]){
+            tObj[x]=tObj[x]+1;
+        }
+        else{
+            tObj[x]=1
+        }
+    });
+    console.log(tObj)
+    console.log(sObj);
+    if(Object.keys(tObj).length===Object.keys(sObj).length){
+        for(let i in tObj){
+            if(tObj[i]!==sObj[i]){
+                return false
+            }
+        };
+    }
+    else{
+        return false
+    }
+    
+    return true
+};
