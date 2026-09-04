273e94f979c47c07acce03dcc8732c95d0fab8e7
Ashik <lllashik2@gmail.com>
2026-09-04 16:10:07 +0530

    Time: 34 ms (46.73%), Space: 69.5 MB (15.52%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -6,16 +6,16 @@ var groupAnagrams = function(strs) {
     if(strs.length===1){
         return [strs]
     };
-    const copy = structuredClone(strs);
-    const sorted = copy.map((item)=>item.split("").sort((a,b)=>a.localeCompare(b)).join(""));
+    // const copy = structuredClone(strs);
+    const sorted = strs.map((item)=>item.split("").sort((a,b)=>a.localeCompare(b)).join(""));
     // console.log(sorted);
     const obj ={};
     sorted.forEach((item,index)=>{
         if(!obj[item]){
-            obj[item] = [copy[index]]
+            obj[item] = [strs[index]]
         }
         else{
-           obj[item].push(copy[index]) 
+           obj[item].push(strs[index]) 
         }
     });
     return Object.values(obj)
