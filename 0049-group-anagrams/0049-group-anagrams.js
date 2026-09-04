fba5598ba0c46695f6858ed10050ce2b8b2f9d7a
Ashik <lllashik2@gmail.com>
2026-09-04 16:08:14 +0530

    Time: 49 ms (12.5%), Space: 70.2 MB (7.05%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    if(strs.length===1){
+        return [strs]
+    };
+    const copy = structuredClone(strs);
+    const sorted = copy.map((item)=>item.split("").sort((a,b)=>a.localeCompare(b)).join(""));
+    console.log(sorted);
+    const obj ={};
+    sorted.forEach((item,index)=>{
+        if(!obj[item]){
+            obj[item] = [copy[index]]
+        }
+        else{
+           obj[item].push(copy[index]) 
+        }
+    });
+    return Object.values(obj)
+};
