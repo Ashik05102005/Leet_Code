0e1c99c6f6606637315d87583185e4b8c6e27df5
Ashik <lllashik2@gmail.com>
2026-09-09 17:38:42 +0530

    Time: 2 ms (86.02%), Space: 59.9 MB (34.41%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.js
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.js
@@ -6,10 +6,10 @@
 var findMedianSortedArrays = function(nums1, nums2) {
     const newArray=[...nums1,...nums2].sort((a,b)=>a-b)
     if(newArray.length%2===0){
-        const num1 = newArray[newArray.length/2];
-        const num2 = newArray[(newArray.length/2)-1];
+        // const num1 = newArray[newArray.length/2];
+        // const num2 = newArray[(newArray.length/2)-1];
         // console.log(num1,num2)
-        return (num1+num2)/2
+        return (newArray[newArray.length/2]+newArray[(newArray.length/2)-1])/2
 
     }
     else{
