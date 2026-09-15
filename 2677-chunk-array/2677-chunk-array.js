370a08dd6dd7741344c9e6a04cae5597e07b15a0
Ashik <lllashik2@gmail.com>
2026-09-15 12:02:01 +0530

    Time: 47 ms (54.44%), Space: 56.5 MB (37.4%) - LeetHub

---
+++ 2677-chunk-array/2677-chunk-array.js
@@ -0,0 +1,20 @@
+/**
+ * @param {Array} arr
+ * @param {number} size
+ * @return {Array}
+ */
+var chunk = function (arr, size) {
+    const res = [];
+    for (let i = 0; i < Math.ceil(arr.length / size); i++) {
+        const array = []
+        for (let j = 0; j < size; j++) {
+            if (arr[i * size + j]!==undefined) {
+                array.push(arr[i * size + j])
+            }
+        };
+        res.push(array)
+
+        // for
+    }
+    return res
+};
