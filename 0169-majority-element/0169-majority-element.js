e02453c4fa647e32e5e025e3f12be41f499c6f44
Ashik <lllashik2@gmail.com>
2026-09-18 11:54:40 +0530

    Time: 11 ms (12.76%), Space: 60.6 MB (5.89%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -2,16 +2,21 @@
  * @param {number[]} nums
  * @return {number}
  */
-var majorityElement = function(nums) {
-    const obj = {};
-    nums.forEach(num=>{
-        if(obj[num]){
-            obj[num]=obj[num]+1
-        }
-        else{
-            obj[num]=1
-        }
-    });
+var majorityElement = function (nums) {
+    // const obj = {};
+    // nums.forEach(num=>{
+    //     if(obj[num]){
+    //         obj[num]=obj[num]+1
+    //     }
+    //     else{
+    //         obj[num]=1
+    //     }
+    // });
+    const obj = nums.reduce((object, item) =>{
+        object[item] = (object[item]||0)+1;
+        return object
+    },{});
+    console.log(obj)
     let top=0;
     let value=Object.values(obj)[0];
     for(let i in obj){
