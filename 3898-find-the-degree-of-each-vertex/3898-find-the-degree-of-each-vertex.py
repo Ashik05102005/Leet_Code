ab163f62cfd997dcdb9a1de8011beb411e9bee22
Ashik <lllashik2@gmail.com>
2026-09-24 16:45:03 +0530

    Time: 8 ms (49.45%), Space: 20 MB (27.82%) - LeetHub

--- 3898-find-the-degree-of-each-vertex/3898-find-the-degree-of-each-vertex.py
+++ 3898-find-the-degree-of-each-vertex/3898-find-the-degree-of-each-vertex.py
@@ -4,9 +4,7 @@ class Solution:
         for list_nums in matrix :
             sum = 0
             for nums in list_nums :
-                # print(nums)
                 sum = sum+nums
-
             res.append(sum)
         return res
             
